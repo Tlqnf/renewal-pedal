bb292970efcb944c267964da835d1244ec7d92cc
Sepro <shtmdwns0@gmail.com>
2026-04-21 22:04:21 +0900

    fix: assets 추가

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pedal/common/theme/app_colors.dart';
 import 'package:pedal/common/theme/app_text_styles.dart';
 
@@ -13,18 +14,20 @@ class AppTheme {
           surface: AppColors.surface,
           error: AppColors.error,
         ),
-        textTheme: TextTheme(
-          headlineLarge: AppTextStyles.tit2xl,
-          headlineMedium: AppTextStyles.titXl,
-          headlineSmall: AppTextStyles.titLg,
-          titleLarge: AppTextStyles.titMd,
-          titleMedium: AppTextStyles.titSm,
-          titleSmall: AppTextStyles.titXs,
-          bodyLarge: AppTextStyles.txtLg,
-          bodyMedium: AppTextStyles.txtMd,
-          bodySmall: AppTextStyles.txtSm,
-          labelLarge: AppTextStyles.txtXs,
-          labelMedium: AppTextStyles.txt2xs,
+        textTheme: GoogleFonts.robotoTextTheme(
+          TextTheme(
+            headlineLarge: AppTextStyles.tit2xl,
+            headlineMedium: AppTextStyles.titXl,
+            headlineSmall: AppTextStyles.titLg,
+            titleLarge: AppTextStyles.titMd,
+            titleMedium: AppTextStyles.titSm,
+            titleSmall: AppTextStyles.titXs,
+            bodyLarge: AppTextStyles.txtLg,
+            bodyMedium: AppTextStyles.txtMd,
+            bodySmall: AppTextStyles.txtSm,
+            labelLarge: AppTextStyles.txtXs,
+            labelMedium: AppTextStyles.txt2xs,
+          ),
         ),
         scaffoldBackgroundColor: AppColors.background,
         dividerColor: AppColors.divider,
